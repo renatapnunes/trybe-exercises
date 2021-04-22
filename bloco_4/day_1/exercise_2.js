ff3f365740c169a15317054a81ed47994080d762
Renata Nunes <renatanunes231@yahoo.com.br>
2021-04-22 16:00:09 -0300

    Exercicio que retorna o maior de dois numeros.

---
+++ bloco_4/day_1/exercise_2.js
@@ -0,0 +1,9 @@
+let a = Math.floor(Math.random() * (100 - 0 +1)) + 0;
+let b = Math.floor(Math.random() * (100 - 0 +1)) + 0;
+
+if (a>b){
+    console.log("A="+a +" maior que B=" +b);
+}
+else {
+    console.log("B="+b +" maior que A=" +a);
+}
