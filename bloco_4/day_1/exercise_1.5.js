153db01182be665aa39a2a08906b2b8bb12253e1
Renata Nunes <renatanunes231@yahoo.com.br>
2021-04-23 00:06:32 -0300

    Arquivos deletados

--- bloco_4/day_1/exercise_1.5.js
+++
@@ -1,5 +0,0 @@
-let a = Math.floor(Math.random() * (100 - 0 +1)) + 0;
-let b = Math.floor(Math.random() * (100 - 0 +1)) + 0;
-
-console.log("Resto de: " +a +"/" +b)
-console.log("Resultado = " +(a%b));
