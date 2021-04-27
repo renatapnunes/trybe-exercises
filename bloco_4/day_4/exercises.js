d68642b3ecce998156a59af3ea0eb11cc5304daf
Renata Nunes <renatanunes231@yahoo.com.br>
2021-04-27 20:06:35 -0300

    Inicio dos exercicios do dia

---
+++ bloco_4/day_4/exercises.js
@@ -0,0 +1,15 @@
+//                               BLOCO 4 - DIA 4 - JavaScript: Objetos e Funções - Exercícios
+// ****************************************************************************************************************************
+
+// -------------------------------------------- Part I: Objetos e For/in ------------------------------------------------------
+
+// Usando o objeto a baixo, faça os exercícios a seguir:
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome:
+
