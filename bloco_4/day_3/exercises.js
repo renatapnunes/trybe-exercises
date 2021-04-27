80ee5081cecd2e46c31229cb04714fba9f90fc91
Renata Nunes <renatanunes231@yahoo.com.br>
2021-04-27 15:48:49 -0300

    Tentando fazer o exercicio 3

--- bloco_4/day_3/exercises.js
+++ bloco_4/day_3/exercises.js
@@ -19,7 +19,7 @@ for(let i = 0; i < n; i += 1){
 let n = Math.ceil(Math.random()*10);
 let linha = "";
 
-for(let j = 0; j < n; j += 1){
+for(let i = 0; i < n; i += 1){
     linha += "*";
     console.log(linha);
 };
@@ -27,14 +27,109 @@ for(let j = 0; j < n; j += 1){
 /* 3 - Agora inverta o lado do triângulo: */
 
 let n = Math.ceil(Math.random()*10);
+let aux = n;
 
-for(let i = 1; i <= n; i += 1) {
+for(let i = 0; i < n; i += 1){
     let linha = "";
-    for(let j = n; j <= 1; j -= 1) {
-        linha += " ";
+    for(let j = 1; j <= n; j += 1){
+        if(j < (aux)){
+            linha += " ";
+        };
+        else{
+            linha += "*";
+        };
     };
-    for(let k = 0; k < n; k += 1) {
+    aux -= 1;
+    console.log(linha);
+};
+
+// 4 - Faça uma pirâmide com n asteriscos de base:
+
+let n = Math.ceil(Math.random()*10);
+
+for(let i = 0; i < n; i += 1){
+    let linha = "";
+    for(let j = 1; j <= n; j += 1) {
+        if(){
+            
+        }
+    };
+};
+
+for(let j = 0; j < n; j += 1){
+    linha += "*";
+    console.log(linha);
+};
+
+
+
+let n = Math.ceil(Math.random()*10);
+let aux = 0;
+
+console.log(n);
+
+for(let i = 0; i < n; i += 1){
+    let linha = "";
+    for(let j = n; j >= 0; j -= 1) {
         linha += "*";
+    };
+    for(let k = 1; k <= aux; k += 1){
+        linha += "o";
+    };
+    aux = 2*aux + 1;
+    console.log(linha);
+};
+
+
+let n = Math.ceil(Math.random()*10);
+let aux = 0;
+
+console.log(n);
+
+for(let j = 0; j < n; j += 1){
+    let linha = "";
+    for(let i = n; i >= 0; i -= 1){
+        if(i > 2*aux+1){
+            linha += "*";
+        }
+        else {
+            linha += "o"
+        };
+    }
+    aux += 1;
+    console.log(linha);
+};
+
+//Invertido --------------------------------
+
+let n = Math.ceil(Math.random()*10);
+let aux = n;
+console.log(n);
+
+for(let i = 0; i < n; i += 1){
+    let linha = "";
+    for(let j = aux; j > 0; j -= 1){
+        linha += "o";
+    }
+    aux -= 1;
+    console.log(linha);
+}
+//------------------------------------------
+ 
+let n = Math.ceil(Math.random()*10);
+let aux = 0;
+console.log(n);
+
+for(let j = 0; j < n; j += 1){
+    let linha = "";
+    for(let i = 1; i <= n; i += 1){
+        if(i <= aux){
+            linha += "*";
+        }
+        else {
+            linha += "o";
+        }
+        aux = 2*aux+1;
     };
     console.log(linha);
 };
