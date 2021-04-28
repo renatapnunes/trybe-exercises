5c2e19740d73f0a5e269b37382a57a781bba2ee9
Renata Nunes <renatanunes231@yahoo.com.br>
2021-04-28 01:44:31 -0300

    Exercicios da Part II - Faltando o 5

---
+++ bloco_4/day_4/exercises_part_2.js
@@ -0,0 +1,167 @@
+//                               BLOCO 4 - DIA 4 - JavaScript: Objetos e Funções - Exercícios
+// ****************************************************************************************************************************
+
+// -------------------------------------------------- Part II: Funções ------------------------------------------------------
+
+// 1 - Crie uma função que receba uma string e retorne true se for um palíndromo , ou false , se não for:
+
+function verificaPalindromo(palavra){
+
+    let palavraInvertida = "";
+
+    for(let i = palavra.length - 1; i >= 0; i -= 1){
+        palavraInvertida += palavra[i];
+    }
+    if(palavraInvertida == palavra){
+        palindromo = true;
+    }
+    else{
+        palindromo = false;
+    };
+
+    return palindromo;
+};
+
+console.log("É palindromo:");
+console.log(verificaPalindromo("arara"));
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+// 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor:
+
+
+function indiceMaiorValor(number) {
+
+    let indiceDoMaior = 0;
+
+    for(let i = 0; i < number.length - 1; i += 1){
+        if(number[i] > number[i+1]){
+            indiceDoMaior = i;
+        }
+        else{
+            indiceDoMaior = i+1;
+        };
+    };
+
+    return indiceDoMaior;
+};
+
+console.log(indiceMaiorValor([2, 3, 6, 7, 10, 1]));
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+// 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor:
+
+function indiceMenorValor(number) {
+
+    let indiceDoMenor = 0;
+
+    for(let i = 0; i < number.length - 1; i += 1){
+        if(number[i] < number[i+1]){
+            indiceDoMenor = i;
+        }
+        else{
+            indiceDoMenor = i+1;
+        };
+    };
+
+    return indiceDoMenor;
+};
+
+console.log(indiceMenorValor([2, 4, 6, 7, 10, 0, -3]));
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+// 4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres:
+
+function maiorNome(nomes) {
+    let maiorQuantidade = "";
+
+    for(let i = 0; i < nomes.length - 1; i += 1) {
+        if(nomes[i].length > nomes[i+1].length){
+            maiorQuantidade = nomes[i];
+        }
+        else{
+            maiorQuantidade = nomes[i+1];
+        };
+    };
+
+    return maiorQuantidade;
+};
+
+console.log(maiorNome(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+// 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete:
+
+function inteiroMaisRepete(inteiros){
+    let inteiroMaisRepetiu = 0;
+    let vezesMaisRepetiu = 0;
+
+    for(let i = 0; i < inteiros.length; i += 1){
+        let vezesInteiroRepetiu = 0;
+
+        for(let j = 0; j < inteiros.length; j += 1){
+            if(inteiros[i] === inteiros[j]){
+                vezesInteiroRepetiu += 1;
+            };
+        };
+
+        console.log("inteiro: " +inteiros[i]);
+        console.log("Vezes que repetiu: " +vezesInteiroRepetiu);
+        console.log("------------------");
+
+        if(vezesInteiroRepetiu > vezesMaisRepetiu){
+            vezesMaisRepitiu = vezesInteiroRepetiu;
+            inteiroMaisRepetiu = inteiros[i];
+        };
+    };
+
+    return inteiroMaisRepetiu;
+};
+
+console.log("Inteiro que mais repetiu: " +inteiroMaisRepete([2, 3, 2, 5, 8, 2, 3]));
+console.log(" --- ");
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+// 6 - Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N:
+
+function somatorio(n){
+    let soma = 0;
+
+    for(let i = 1; i <= n; i +=1){
+        soma += i;
+    };
+
+    return soma;
+};
+
+console.log(somatorio(5));
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+/* 7 - Crie uma função que receba uma string word e outra string ending .
+Verifique se a string ending é o final da string word .
+Considere que a string ending sempre será menor que a string word . */
+
+function verificaFimPalavra(word, ending){
+    let finalDaStringWord;
+    let aux = 0;
+    for(let i = (word.length - ending.length); i < word.length; i += 1){
+        if(word[i] === ending[aux]){
+            finalDaStringWord = true;
+        }
+        else {
+            finalDaStringWord = false;
+            i = word.length;
+        };
+        aux += 1;
+    };
+    return finalDaStringWord;
+};
+
+console.log(verificaFimPalavra("trybe", "be"));
+
+
