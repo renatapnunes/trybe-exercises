95daf84816c477719d236165fe192f00aaf32b97
Renata Nunes <renatanunes231@yahoo.com.br>
2021-05-21 01:20:41 -0300

    Exercicios 1 a 8 da Part II - Concluidos

--- bloco_7/day_2/exercises1.js
+++ bloco_7/day_2/exercises1.js
@@ -38,6 +38,13 @@ const order = {
     },
   };
 
+
+/* Exercicio 1
+Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira,
+entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
+    - Note que o parâmetro da função já está sendo passado na chamada da função. */
+
+
   const customerInfo = (order) => {
     // Adicione abaixo as informações necessárias.
     console.log(`Olá ${order.order.delivery.deliveryPerson},
@@ -46,7 +53,15 @@ const order = {
   }
   
   customerInfo(order);
-  
+
+
+/* Exercicio 2
+Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de muzzarella,
+calabresa e Coca-Cola Zero é R$ 50,00."
+    - Modifique o nome da pessoa compradora.
+    - Modifique o valor total da compra para R$ 50,00. */
+
+    
   const orderModifier = (order) => {
     // Adicione abaixo as informações necessárias.
     order.name = 'Luiz Silva';
@@ -66,14 +81,3 @@ const order = {
   }
   
   orderModifier(order);
-
-/* Exercicio 1
-Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira,
-entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
-    - Note que o parâmetro da função já está sendo passado na chamada da função. */
-
-/* Exercicio 2
-Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de muzzarella,
-calabresa e Coca-Cola Zero é R$ 50,00."
-    - Modifique o nome da pessoa compradora.
-    - Modifique o valor total da compra para R$ 50,00. */
